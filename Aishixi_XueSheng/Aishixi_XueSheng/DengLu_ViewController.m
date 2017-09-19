d230254bd39b79a827f5d1626cbc6b58fd4988f0
斌小狼 <271377321@qq.com>
2017-09-19 13:39:34 +0800

    加了点封装

---
+++ Aishixi_XueSheng/Aishixi_XueSheng/DengLu_ViewController.m
@@ -0,0 +1,29 @@
+//
+//  DengLu_ViewController.m
+//  Aishixi_XueSheng
+//
+//  Created by 斌小狼 on 2017/9/18.
+//  Copyright © 2017年 GuoYun. All rights reserved.
+//
+
+#import "DengLu_ViewController.h"
+
+@interface DengLu_ViewController ()
+
+@end
+
+@implementation DengLu_ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
